41cfda930cdba0beb5f43e1b725e1334c1e88b4d
gabrielcannes <gabrielcannes.gc@gmail.com>
2024-05-10 14:18:02 -0300

    New player component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,23 @@
 import { Routes } from '@angular/router';
 import { LoginModule } from './pages/login/login.module';
+import { PlayerModule } from './pages/player/player.module';
 
 export const AppRoutes: Routes = [
+  //quando o caminho da URL estiver vazio, redirecionar para a rota de login
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+
+  // usando loadChildren para chamar o módulo fazemos com que a pagina de login só seja carregada quando
+  // o usuário for realmente acessar a rota
   {
     path: 'login',
     loadChildren: () => LoginModule,
+  },
+  {
+    path: 'player',
+    loadChildren: () => PlayerModule,
   },
 ];
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from 'src/app/services/spotifyService/spotify.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  constructor(private spotifyService: SpotifyService) {}
 
+  ngOnInit(): void {
+    this.verificarTokenUrlCallback();
+  }
+
+  verificarTokenUrlCallback() {
+    const token = this.spotifyService.obterTokenUrlCallback();
+    if (!!token) {
+      this.spotifyService.definirAccessToken(token);
+    }
+  }
+
+  openLoginPage() {
+    window.location.href = this.spotifyService.obterUrlLogin();
+  }
 }
